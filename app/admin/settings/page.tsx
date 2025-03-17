d32ea67d27b36e8b89c6939d858810118acab1d2
Zachary Gulick <zgulick@gmail.com>
2025-03-17 14:38:02 +0000

    fixing typescript errors2

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import { Save, Info, Loader2} from 'lucide-react';
+import { Save, Info } from 'lucide-react';
 import api from '@/lib/api';
 
 export default function SettingsPage() {
